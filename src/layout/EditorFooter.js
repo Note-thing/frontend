ba080ae4cac7c827e5225dfbd5ac599f3e4161d8
Ilias <igoujgali@gmail.com>
2021-11-02 14:43:22 +0100

    Explode app_layout into several components

---
+++ src/layout/EditorFooter.js
@@ -0,0 +1,35 @@
+import {
+    Grid,
+    Chip,
+} from "@mui/material";
+import "./AppLayout.css";
+import {
+    LocalOffer,
+} from "@mui/icons-material";
+
+/**
+ * Editor Footer. Allows the user to add label to his note
+ * @returns 
+ */
+export default function EditorFooter(){
+    const tags = ["Compound litteral", "RHH", "Blanc"];
+    return(
+        <Grid
+        display="flex"
+        alignItems="center"
+        minHeight="4rem"
+        padding="0 1rem 0 1rem"
+        borderTop="0.1rem solid #e9F0F0"
+        className="editor-tag-footer"
+    >
+        <LocalOffer />{" "}
+        {tags.map((tag) => (
+            <Chip
+                className="tag-chip"
+                label={tag}
+                onDelete={() => console.log("delete" + { tag })}
+            />
+        ))}
+    </Grid>
+    )
+}
---
+++ src/layout/EditorHeader.js
@@ -0,0 +1,49 @@
+import {
+    Grid,
+    Input,
+} from "@mui/material";
+import "./AppLayout.css";
+import {
+    Code,
+    Preview,
+    VerticalSplit,
+    PictureAsPdf,
+    Share,
+    Delete,
+} from "@mui/icons-material";
+
+/**
+ * Header of the editor containing the note menu (display switch, PDF export, delete the note etc.). 
+ * @returns 
+ */
+export default function EditorHeader() {
+    return (
+        <Grid
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            minHeight="4rem"
+            padding="0 1rem 0 1rem"
+            borderBottom="0.1rem solid #e9f0f0"
+            backgroundColor="#f5f9f9"
+        >
+            <Grid display="flex" justifyContent="space-around" width="10%">
+                <Code className="menu-icon-item" />
+                <Preview className="menu-icon-item" />
+                <VerticalSplit className="menu-icon-item" />
+            </Grid>
+
+            <Input
+                sx={{ width: "30rem", fontSize: "1.2rem" }}
+                value=" Guide de survie durant le III Rentsch"
+                placeholder="Titre de la note"
+            />
+
+            <Grid display="flex" justifyContent="space-around" width="10%">
+                <Share className="menu-icon-item" />
+                <PictureAsPdf className="menu-icon-item" />
+                <Delete className="menu-icon-item" />
+            </Grid>
+        </Grid>
+    );
+}
