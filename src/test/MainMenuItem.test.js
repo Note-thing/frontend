5920124ea7bd2951a47c1f6c61cbbd084e434b3b
Ilias <igoujgali@gmail.com>
2021-11-11 09:07:59 +0100

    Failed test

--- src/test/MainMenuItem.test.js
+++ src/test/MainMenuItem.test.js
@@ -82,6 +82,6 @@ it('MainMenuItem should display (opacity = 1, height : auto) notes on click', ()
     });
 });
 it('Presentation CI/CD', () => {
-    //expect('bonjour').toBe("au revoir")
-    expect('bonjour').toBe("bonjour")
-})
+    
+    expect('bonjour').toBe('bonjour');
+});
