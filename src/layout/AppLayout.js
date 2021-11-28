2efb9b38fc7f02099987454a460e53656c7b270f
Ovich <stefanteofanovic@hotmail.com>
2021-11-28 02:53:38 +0100

    Theme

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -11,20 +11,19 @@ import { NoteProvider } from '../context/NoteContext';
  * @param {*} props
  * @returns
  */
-export default function AppLayout(props) {
+export default function AppLayout() {
     return (
         <NoteProvider>
             <Grid container height="100vh">
                 <Grid
                     item
-                    xs={4}
-                    md={2}
+                    xs={3}
                     height="100%"
                     borderRight="0.1rem solid #e9F0F0"
                 >
                     <MainMenu />
                 </Grid>
-                <Grid item xs={8} md={10} height="100%">
+                <Grid item xs={9} height="100%">
                     <Switch>
                         <Route path="/directory/**/note/**" component={EditorComponent} />
                     </Switch>
