95a5773c3811185daeeedd8924c741233eac887c
Ovich <stefanteofanovic@hotmail.com>
2021-12-16 12:37:03 +0100

    Edit

--- src/context/NoteContext.js
+++ src/context/NoteContext.js
@@ -33,114 +33,78 @@ const data = {
                 uniqid: 'dfgh3245sdfg',
                 title: 'CSS',
                 tags: ['Web', 'design'],
-                content: `CD  seems to work with Github Acti&#128169;!!! <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very popular
-            during the Renaissance. The first line of Lorem Ipsum, "Lorem
-            ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random
-            text. It has roots in a piece of classical Latin literature from
-            45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very popular
-            during the Renaissance. The first line of Lorem Ipsum, "Lorem
-            ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random
-            text. It has roots in a piece of classical Latin literature from
-            45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very
-            popularThis book is a treatise on the theory of ethics, very
-            popular during the Renaissance. The first line of Lorem Ipsum,
-            "Lorem ipsum dolor sit amet..", comes from a line in section
-            1.10.32. Contrary to popular belief, Lorem Ipsum is not simply
-            random text. It has roots in a piece of classical Latin
-            literature from 45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very popular
-            during the Renaissance. The first line of Lorem Ipsum, "Lorem
-            ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random
-            text. It has roots in a piece of classical Latin literature from
-            45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very
-            popularThis book is a treatise on the theory of ethics, very
-            popular during the Renaissance. The first line of Lorem Ipsum,
-            "Lorem ipsum dolor sit amet..", comes from a line in section
-            1.10.32. Contrary to popular belief, Lorem Ipsum is not simply
-            random text. It has roots in a piece of classical Latin
-            literature from 45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very popular
-            during the Renaissance. The first line of Lorem Ipsum, "Lorem
-            ipsum dolor sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random
-            text. It has roots in a piece of classical Latin literature from
-            45 BC, making it over 2000 years old.
-            <br />
-            <br />
-            Richard McClintock, a Latin professor at Hampden-Sydney College
-            in Virginia, looked up one of the more obscure Latin words,
-            consectetur, from a Lorem Ipsum passage, and going through the
-            cites of the word in classical literature, discovered the
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
-            1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
-            and Evil) by Cicero, written in 45 BC.
-            <br />
-            This book is a treatise on the theory of ethics, very popular`
+                content: `# Getting Started with Create React App
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).              
+NPM installs
+\`\`\`javascript
+npm install topbar --save
+npm install react-html-parser
+\`\`\`
+## Available Scripts
+In the project directory, you can run:
+### \`npm start\`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### \`npm test\`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### \`npm run build\`
+
+Builds the app for production to the \`build\` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### \`npm run eject\`
+
+**Note: this is a one-way operation. Once you \`eject\`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can \`eject\` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except \`eject\` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use \`eject\`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### \`npm run build\` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+`
             },
             { uniqid: 'awei546fcguuz', title: 'JS', tags: ['JS', 'prototype'] },
             { uniqid: '345jfhtzdffvret', title: 'Node', tags: ['JS', 'SSR'] }
--- src/layout/editor/EditorComponent.js
+++ src/layout/editor/EditorComponent.js
@@ -1,18 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Grid } from '@mui/material';
 import ReactHtmlParser from 'react-html-parser';
+import TextareaMarkdown from 'textarea-markdown';
 import EditorFooter from './EditorFooter';
 import EditorHeader from './EditorHeader';
 import { NoteContext } from '../../context/NoteContext';
+import { useInput } from '../../hooks/useInput';
+
 export default function EditorComponent() {
     const { notes: { note: { content } } } = useContext(NoteContext);
+    const { value: noteTextArea, bind: bindNoteTextArea } = useInput(content);
+
+    useEffect(() => {
+        const textarea = document.querySelector('textarea#editor');
+        textarea.value = content;
+        new TextareaMarkdown(textarea);
+    }, [content]);
+
     return (
         <Grid container direction="column" height="100%">
             <Grid item sx={{ height: '7%' }}>
                 <EditorHeader />
             </Grid>
-            <Grid sx={{ padding: '20px', height: '86%', overflowY: 'scroll' }}>
-                { content && ReactHtmlParser(content) }
+            <Grid item sx={{ p: '20px', height: '86%', overflowY: 'scroll' }}>
+                <textarea
+                    id="editor"
+                    data-preview="#preview"
+                    {...bindNoteTextArea}
+                />
+                <h2>Preview</h2>
+                <div id="preview" />
             </Grid>
             <Grid item sx={{ height: '7%' }}>
                 <EditorFooter />
